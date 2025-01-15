5d7ddaec153f7b564820559fa3fd03ef1b904428
ZoharFrancoGrip <zohar.franco@grip.security>
2025-01-15 09:49:40 +0200

    Add app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React, { useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import TasksDashboard from "./pages/TasksDashboard";
+import { Box, IconButton } from "@mui/material";
+import { Brightness2Rounded, Brightness7 } from "@mui/icons-material";
+
+const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      
+      darkLight: {
+        blue: darkMode ? "#38b3f1" : "blue",
+        red: darkMode ? "#f84040" : "red",
+        green: darkMode ? "#47f347" : "green",
+        orange: darkMode ? "#f1a538" : "orange",
+        yellow: darkMode ? "#f1f138" : "yellow",
+      },
+
+      background: {
+        default: darkMode ? "#121212" : "#f0f0f0",
+      },
+      text: {
+        primary: darkMode ? "#f0f0f0" : "#121212",
+      },
+      primary: {
+        main: darkMode ? "#f0f0f0" : "#121212",
+      },
+      secondary: {
+        main: darkMode ? "#f0f0f0" : "#121212",
+      },
+      
+      
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <IconButton onClick={() => setDarkMode(!darkMode)} sx={{position: 'absolute', top: 10, right: 10}}  >
+          {darkMode ? <Brightness7 /> : <Brightness2Rounded />}
+        </IconButton>
+        <Box sx={{height: '100vh'}}><TasksDashboard /></Box>;
+     
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
